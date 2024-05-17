2698118f77e7fe7bfe00f29af1be706611e4b04b
Zed <108571201+zedifen@users.noreply.github.com>
2024-05-17 11:20:40 +0800

    Exceeding CPU time is fixed so make it semantic

--- worker.ts
+++ worker.ts
@@ -95,12 +95,7 @@ export default {
       return new Response(messages.join('\n\n'), {status: 400});
     }
 
-    const slices: [number, number, number][] = [];
-    const texts: Uint8Array[] = [];
-    let slicesTotalLength = 0;
-    let textN = 0;
-    const returnCharCode = '\n'.charCodeAt(0);
-    const dotCharCode = '.'.charCodeAt(0);
+    const servers: string[][] = [];
 
     // fetching from upstream
     for (const n of (
@@ -109,34 +104,14 @@ export default {
       : [name]
     )) {
       const src = await fetch(`${UPSTREAM_SOURCES_ROOT}${n}.conf`);
-      const text = new Uint8Array(await src.arrayBuffer());
+      const text = await src.text();
       // pre-processing response
-      let i = 0;
-      const slashCode = '/'.charCodeAt(0);
-      const matcher = 'server'.split('').map(i => i.charCodeAt(0));
-      function match(text: Uint8Array, a: number, matcher: number[]) {
-        for (let i = 0; i < matcher.length; i += 1) {
-          if (text[a + i] != matcher[i]) return false;
-        }
-        return true;
-      }
-      while (i < text.length) {
-        if (match(text, i, matcher)) {
-          i += matcher.length;
-          while (text[i] != slashCode /* && i < text.length && text[i] != '\n'.charCodeAt(0) */) i += 1;
-          i += 1;
-          const a = i;
-          while (text[i] != slashCode /* && i < text.length && text[i] != '\n'.charCodeAt(0) */) i += 1;
-          const b = i;
-          i += 1;
-          slices.push([textN, a, b]);
-          slicesTotalLength += (b - a);
-        }
-        while (text[i] != returnCharCode && i < text.length) i += 1;
-        i += 1;
-      }
-      texts.push(text);
-      textN += 1;
+      servers.push(
+        text
+          .split('\n')
+          .filter(i => i.startsWith('server'))
+          .map(m => m.slice(m.indexOf('/') + 1, m.lastIndexOf('/')))
+      );
     }
 
     // generating response for one of the formats (determined by suffix)
@@ -153,51 +128,24 @@ export default {
 
     // handle resource generating for different formats (suffixes)
     switch (suffix) {
-      case '.txt': {
-        const a = new Uint8Array(slicesTotalLength + slices.length);
-        let i = 0;
-        for (const [n, x, y] of slices) {
-          for (let q = x; q < y; q += 1) { a[i++] = texts[n][q]; }
-          a[i++] = returnCharCode;
-        }
-        return putCache(new Response(a, {headers}));
-      }
+      case '.txt':
+        return putCache(new Response(servers.flat().join('\n'), {headers}));
       case '.conf': {
-        const prefix = 'DOMAIN-SUFFIX,'.split('').map(i => i.charCodeAt(0));
-        const a = new Uint8Array(slicesTotalLength + (prefix.length + 1) * slices.length);
-        let i = 0;
-        for (const [n, x, y] of slices) {
-          for (const c of prefix) { a[i++] = c; }
-          for (let q = x; q < y; q += 1) { a[i++] = texts[n][q]; }
-          a[i++] = returnCharCode;
-        }
-        return putCache(new Response(a, {headers}));
+        return putCache(new Response(
+          'DOMAIN-SUFFIX,' + servers.flat().join('\nDOMAIN-SUFFIX,') + '\n',
+          { headers },
+        ));
       }
       case '.list': {
-        const a = new Uint8Array(slicesTotalLength + 2 * slices.length);
-        let i = 0;
-        for (const [n, x, y] of slices) {
-          a[i++] = dotCharCode;
-          for (let q = x; q < y; q += 1) { a[i++] = texts[n][q]; }
-          a[i++] = returnCharCode;
-        }
-        return putCache(new Response(a, {headers}));
+        return putCache(new Response('.' + servers.flat().join('\n.') + '\n', {headers}));
       }
       case '.hosts': {
-        const s = '[Host]\n\n'.split('').map(i => i.charCodeAt(0));
-        const p = ' = server:system\n'.split('').map(i => i.charCodeAt(0));
-        const q = '+.'.split('').map(i => i.charCodeAt(0));
-        const a = new Uint8Array(s.length + slicesTotalLength * 2 + (2 * p.length + q.length) * slices.length);
-        let i = 0;
-        for (const c of s) { a[i++] = c; }
-        for (const [n, x, y] of slices) {
-          for (let q = x; q < y; q += 1) { a[i++] = texts[n][q]; }
-          for (const c of p) { a[i++] = c; }
-          for (const c of q) { a[i++] = c; }
-          for (let q = x; q < y; q += 1) { a[i++] = texts[n][q]; }
-          for (const c of p) { a[i++] = c; }
-        }
-        return putCache(new Response(a, {headers}));
+        return putCache(new Response(
+          '[Host]\n\n' + servers.flat().map(n => 
+            `${n} = server:system\n` +
+          `+.${n} = server:system\n`).join(''),
+          { headers },
+        ));
       }
       default:
         return new Response('never');
